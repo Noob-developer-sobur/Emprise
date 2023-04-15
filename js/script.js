4b27a68f9ea24e62f9beec23ca452f5e124c2b56
Noob-developer-sobur <soburkhan210@gmail.com>
2023-04-15 16:26:14 +0600

    Emprise

---
+++ js/script.js
@@ -0,0 +1,60 @@
+$(function(){
+    
+  $('.catagory-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    arrows: true,
+    prevArrow:".catagory-left-button",
+    nextArrow:".catagory-right-button",
+  });
+	
+})
+// Select Category slider end
+
+$(function(){
+    
+  $('.trending-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+    prevArrow:".left",
+    nextArrow:".right",
+  });
+	
+})
+// Trending 2021 slider end
+$(function(){
+    
+  $('.top-destination-img-three').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+    prevArrow:".left-two",
+    nextArrow:".right-three",
+  });
+	
+})
+
+// Trending 2021 slider end
+
+$(function(){
+    
+  $('.tour-guide-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+    prevArrow:".tour-one",
+    nextArrow:".tour-two",
+  });
+	
+})
+
+// Tour Guides slider end
